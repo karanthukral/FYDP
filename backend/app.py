fd14a7d035f77f2092999b7b3b8c130dfc30b7aa
Karan Thukral <karanthukral@gmail.com>
2016-11-05 14:07:56 -0400

    Added params to list all domains

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 import os
 
 app = Flask(__name__)
@@ -16,7 +17,18 @@ def hello():
 @app.route('/api/v1.0/traffic/list', methods=['GET'])
 def get_tasks():
     if request.method == "GET":
-        traffic = Traffic.query.order_by(Traffic.created_at)
+        only_flagged = request.args.get('only_flagged')
+        min_created_at = request.args.get('min_created_at')
+        if min_created_at == None:
+            created_after = datetime.strptime("1990-09-30 12:34:33", "%Y-%m-%d %H:%M:%S")
+        else:
+            created_after = datetime.strptime(min_created_at, "%Y-%m-%d %H:%M:%S")
+
+        if only_flagged:
+            traffic = Traffic.query.filter_by(flagged=True).filter(Traffic.created_at > created_after).order_by(Traffic.created_at)
+        else:
+            traffic = Traffic.query.filter(Traffic.created_at > created_after).order_by(Traffic.created_at)
+
         return jsonify(traffic_objs=[t.serialize for t in traffic.all()])
     else:
         return "You done goofed"
@@ -30,6 +42,14 @@ def tempcreate():
             )
     db.session.add(traffic)
     db.session.commit()
+
+@app.route('/api/v1.0/traffic/classify', methods=['POST'])
+def classfify():
+    if request == "POST":
+        domain = str(request.form['domain'])
+    else:
+        return "You done goofed"
+
 
 if __name__ == '__main__':
     app.run()
