88883e0320740ce03db2a35f19460f9b631c2523
Karan Thukral <karanthukral@gmail.com>
2017-02-22 11:31:13 -0500

    Updated Traffic model for new data

--- backend/app.py
+++ backend/app.py
@@ -30,48 +30,27 @@ def hello():
 @app.route('/api/v1.0/traffic/list', methods=['GET'])
 def get_tasks():
     if request.method == "GET":
-        min_prob_bad = request.args.get('min_prob_bad')
         min_created_at = request.args.get('min_created_at')
         if min_created_at == None:
             created_after = datetime.strptime("1990-09-30 12:34:33", "%Y-%m-%d %H:%M:%S")
         else:
             created_after = datetime.strptime(min_created_at, "%Y-%m-%d %H:%M:%S")
 
-        if min_prob_bad == None:
-            traffic = Traffic.query.filter(Traffic.created_at > created_after).order_by(Traffic.created_at)
-        else:
-            traffic = Traffic.query.filter_by(prob_bad >= min_prob_bad).filter(Traffic.created_at > created_after).order_by(Traffic.created_at)
+        traffic = Traffic.query.filter(Traffic.created_at > created_after).order_by(Traffic.created_at)
 
         return jsonify(traffic_objs=[t.serialize for t in traffic.all()])
     else:
         return "You done goofed"
 
-@app.route('/tmp/create')
-def tempcreate():
-    traffic = Traffic(
-            human_id="facebook.com",
-            prob_bad=0.5,
-            ext_metadata={}
-            )
-    db.session.add(traffic)
-    db.session.commit()
-
-@app.route('/api/v1.0/traffic/classify', methods=['POST'])
-def classfify():
-    if request.method == "POST":
-        domain = str(request.form['domain'])
-        evaluated = nn.evaluate_domain(domain)
-        traffic = Traffic(
-                human_id=domain,
-                prob_bad = float(evaluated[0]),
-                ext_metadata = {}
-                )
-        db.session.add(traffic)
-        db.session.commit()
-        return jsonify(traffic.serialize)
-    else:
-        return "You done goofed"
-
+# @app.route('/tmp/create')
+# def tempcreate():
+    # traffic = Traffic(
+            # human_id="facebook.com",
+            # prob_bad=0.5,
+            # ext_metadata={}
+            # )
+    # db.session.add(traffic)
+    # db.session.commit()
 
 if __name__ == '__main__':
     app.run()
--- backend/models/traffic.py
+++ backend/models/traffic.py
@@ -11,16 +11,40 @@ class Traffic(db.Model):
     __tablename__ = 'traffic'
 
     id = db.Column(db.Integer, primary_key=True)
-    human_id = db.Column(db.String)
     created_at = db.Column(db.DateTime)
     updated_at = db.Column(db.DateTime)
-    prob_bad = db.Column(db.Float)
-    ext_metadata = db.Column(JSON)
 
-    def __init__(self, human_id, prob_bad, ext_metadata):
-        self.human_id = human_id
-        self.prob_bad = prob_bad
-        self.ext_metadata = ext_metadata if ext_metadata else {}
+    # Source Details
+    total_src_bytes = db.Column(db.Integer)
+    total_src_pkts = db.Column(db.Integer)
+    src_b64_payload = db.Column(db.String)
+    src_utf_payload = db.Column(db.String)
+    src_tcp_flags = db.Column(db.String)
+    src_ip = db.Column(db.String)
+    src_port = db.Column(db.String)
+
+    # Destination Details
+    total_dst_bytes = db.Column(db.Integer)
+    total_dst_pkts = db.Column(db.Integer)
+    dst_b64_payload = db.Column(db.String)
+    dst_utf_payload = db.Column(db.String)
+    dst_tcp_flags = db.Column(db.String)
+    dst_ip = db.Column(db.String)
+    dst_port = db.Column(db.String)
+
+    # Common Details
+    app_name = db.Column(db.String)
+    direction = db.Column(db.String)
+    start_time = db.Column(db.DateTime)
+    end_time = db.Column(db.DateTime)
+    tag = db.Column(db.String)
+
+    # ML Tag
+    classified_tag = db.Column(db.String)
+
+    # ext_metadata = db.Column(JSON)
+
+    def __init__(self):
         self.created_at = datetime.datetime.now()
         self.updated_at = datetime.datetime.now()
 
@@ -31,11 +55,28 @@ class Traffic(db.Model):
     def serialize(self):
         return {
                 'id': self.id,
-                'human_id': self.human_id,
-                'prob_bad': self.prob_bad,
                 'created_at': dump_datetime(self.created_at),
                 'updated_at': dump_datetime(self.updated_at),
-                'ext_metadata': self.ext_metadata
+                'total_src_bytes': self.total_src_bytes,
+                'total_src_pkts': self.total_src_pkts,
+                'src_b64_payload': self.src_b64_payload,
+                'src_utf_payload': self.src_utf_payload,
+                'src_tcp_flags': self.src_tcp_flags,
+                'src_ip': self.src_ip,
+                'src_port': self.src_port,
+                'total_dst_bytes': self.total_dst_bytes,
+                'total_dst_pkts': self.total_dst_pkts,
+                'dst_b64_payload': self.dst_b64_payload,
+                'dst_utf_payload': self.dst_utf_payload,
+                'dst_tcp_flags': self.dst_tcp_flags,
+                'dst_ip': self.dst_ip,
+                'dst_port': self.dst_port,
+                'app_name': self.app_name,
+                'direction': self.direction,
+                'start_time': dump_datetime(self.start_time),
+                'end_time': dump_datetime(self.end_time),
+                'tag': self.tag,
+                'classified_tag': self.classified_tag,
         }
 
 
