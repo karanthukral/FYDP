0490324f6d171390bbb76a1a0b5d691998b220dc
Erik Derohanian <erik@erik-j.de>
2017-03-12 17:04:56 -0400

    simulation almost done

--- sim.py
+++ sim.py
@@ -1,7 +1,7 @@
-import psycopg2
+import pickle, os, random, time
+from datetime import datetime
 
-import os
-import random
+import psycopg2
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
@@ -9,7 +9,7 @@ from sklearn.ensemble import RandomForestClassifier
 DATA_DIR = 'local_data/features/'
 
 def setup_db():
-    print('Connecting to DB')
+    print('Connecting to DB...')
     conn = psycopg2.connect(host="localhost",database="inara", user="inara", password="")
     cursor = conn.cursor()
 
@@ -19,7 +19,7 @@ def setup_db():
     # display the PostgreSQL database server version
     db_version = cursor.fetchone()
     print(db_version)
-
+    print('DB Ready')
     return cursor
 
 def insert(cursor, values):
@@ -32,6 +32,9 @@ created_at, updated_at) VALUES(%s) RETURNING id;"""
     cursor.execute(sql, values)
 
 def make_model():
+
+    print('Beginning model training...')
+    start = time.time()
     X = np.load(os.path.join(DATA_DIR, 'features-new.npy'))
     y = np.load(os.path.join(DATA_DIR, 'labels-new.npy'))[0]
 
@@ -41,19 +44,28 @@ def make_model():
 
     # Fit the model
     clf.fit(X_train, y_train)
-    return clf
+    print('Model training done, took ' + str(time.time()-start) + ' seconds')
+    return [clf, X]
 
 def classify(clf, data):
     return clf.predict(data)
 
-def iterate(clf, cursor):
-    pass
-
+def iterate(clf, X, cursor):
+    full_db_data = pickle.load(open(os.path.join(DATA_DIR,'flow-for-db.pkl'),'rb'))
+    for idx, data in enumerate(X):
+        tag = classify(clf, [data])
+        db_datum = full_db_data[idx]
+        db_datum.append(tag)
+        db_datum.append([datetime.now().strftime("%Y-%m-%d"), datetime.now().strftime("%H:%M:%S")])
+        db_datum.append([datetime.now().strftime("%Y-%m-%d"), datetime.now().strftime("%H:%M:%S")])
+        print("Inserting " + str(db_datum))
+        insert(cursor, db_datum)
+        time.sleep(random.random())
 
 if __name__ == "__main__":
     cursor = setup_db()
-    clf = make_model()
-    iterate(clf, cursor)
+    clf, X = make_model()
+    iterate(clf, X, cursor)
 
     # close connection
     cursor.close()
